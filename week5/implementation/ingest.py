a379a383e3380af4106e4ef419c99265bc459ddb
jharrismiller <36432119+jharrismiller@users.noreply.github.com>
2026-01-31 16:53:36 -0500

    update models and settings

--- week5/implementation/ingest.py
+++ week5/implementation/ingest.py
@@ -15,11 +15,11 @@ MODEL = "gpt-4.1-nano"
 DB_NAME = str(Path(__file__).parent.parent / "vector_db")
 KNOWLEDGE_BASE = str(Path(__file__).parent.parent / "knowledge-base")
 
-# embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 load_dotenv(override=True)
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+#embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
 
 def fetch_documents():
@@ -38,7 +38,7 @@ def fetch_documents():
 
 
 def create_chunks(documents):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=350)
     chunks = text_splitter.split_documents(documents)
     return chunks
 
